a86f17f83e833597bc98c233f4cdc3d45b793194
petr <petr@lqd.cz>
2020-11-05 11:21:07 +0100

    loginform check class

--- src/Control/LoginForm.php
+++ src/Control/LoginForm.php
@@ -40,8 +40,7 @@ class LoginForm extends \Nette\Application\UI\Form
 		
 		$this->user = $user;
 		
-		//@TODO overit typ class
-		if (!isset($class)) {
+		if (!isset($class) || !is_subclass_of($class,IUser::class) || !is_subclass_of($class,Nette\Security\IIdentity::class)) {
 			throw new \DomainException('Wrong or empty class!');
 		}
 		
