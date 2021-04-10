bb59de9fc1481275aa70954985b28144b9922193
Oldrich <oldrich@lqd.cz>
2021-04-10 10:44:44 +0200

    update permissions model

--- src/DB/UserRepositoryTrait.php
+++ src/DB/UserRepositoryTrait.php
@@ -12,7 +12,7 @@ trait UserRepositoryTrait
 {
 	public function getByAccountLogin(string $login): ?IUser
 	{
-		return $this->many()->where('account.login', $login)->select(['role' => 'account.fk_role'])->first();
+		return $this->many()->where('accounts.login', $login)->select(['role' => 'accounts.fk_role'])->first();
 	}
 	
 	public function getByEmail(string $email): ?IUser
